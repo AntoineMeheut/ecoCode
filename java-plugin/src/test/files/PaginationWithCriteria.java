46bad6c35bea685760f57f11780b942707eaab0c
sebastienlesausse <slesausse@gmail.com>
2023-04-06 11:30:21 +0200

    Create test file with Criteria

---
+++ java-plugin/src/test/files/PaginationWithCriteria.java
@@ -0,0 +1,25 @@
+import javax.persistence.criteria;
+import java.util.list;
+
+
+public class PaginationWithCriteria {
+
+    private static final int PAGE_SIZE = 10;
+    private static final int FIRST_RESULT = 10;
+
+     public List<Foo> getFirstPage() { //Compliant
+         Criteria criteria = session.createCriteria(Foo.class);
+         criteria.setFirstResult(FIRST_RESULT);
+         criteria.setMaxResults(PAGE_SIZE);
+         List<Foo> firstPage = criteria.list();
+         return firstPage;
+     }
+
+     public List<Foo> getList() { //Noncompliant {{set first result and set max result to avoid retrieve all data}}
+         Criteria criteria = session.createCriteria(Foo.class);
+         List<Foo> result = criteria.list();
+         return result;
+     }
+
+
+}
